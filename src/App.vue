dd8994390071e63bca4e5b7b8be6ae75b8b9a5cc
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2024-03-23 14:22:31 +0700

    em

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<script >
+import dm from './components/dm.vue'
+export default{
+  name:'App',
+  components:{
+    dm,
+  }
+}
+</script>
+
+<template>
+  <dm />
+</template>
+
+<style >
+
+</style>
